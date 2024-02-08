b4a009e0ed95e3522a0c78f7578569e815faa09a
SWCL0 <stan.w.c@icloud.com>
2024-02-08 11:21:13 +0100

    Update pyramide.php
    Toevoeging commentaar regels

--- pyramide.php
+++ pyramide.php
@@ -1,11 +1,17 @@
 <?php
 
+// Opvragen stapels gebruiker
 echo "Hoeveel stapels wil je hebben?" . PHP_EOL;
 $stapel = readline();
+
+// Berekenen buitenste loop
 for ($i = 1; $i <= $stapel; $i++) {
+    // Berekenen binnenste loop
     for ($j = 1; $j <= $i; $j++) {
+        // Print * in 1 laag
         echo "* ";
     }
+    // Volgende laag
     echo "" . PHP_EOL;
 }
 
